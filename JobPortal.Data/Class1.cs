93197e8a5e9db7c4c4034361546d7222fa94688c
salmap <salmannaqvi@nimapinfotech.com>
2023-03-25 12:06:27 +0530

    Add project files.

---
+++ JobPortal.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JobPortal.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ JobPortal.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JobPortal.Service
+{
+    public class Class1
+    {
+    }
+}
