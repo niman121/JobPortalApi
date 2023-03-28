751c514074973b17367d82d4ca1b5b892921f627
salmap <salmannaqvi@nimapinfotech.com>
2023-03-28 17:43:57 +0530

    sallu bhai

--- JobPortal/Startup.cs
+++ JobPortal/Startup.cs
@@ -1,7 +1,9 @@
+using JobPortal.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace JobPortal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<JobDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("jobPortalDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
