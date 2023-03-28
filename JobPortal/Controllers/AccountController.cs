226cbd223d359a0db5b29d3b536d42a5568868fd
HOME <sabbas240@gmail.com>
2023-03-28 17:49:44 +0530

    added acount controllers

---
+++ JobPortal/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using JobPortal.Service.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+
+        }
+
+        public ActionResult login(SignUpDto dto)
+        {
+            return Ok();
+        }
+        public ActionResult SignUp(SignUpDto dto)
+        {
+            return Ok();
+        }
+
+        public ActionResult PasswordReset()
+        {
+            return Ok();
+        }
+
+        public ActionResult ForgotPassword()
+        {
+            return Ok();
+        }
+    }
+}
